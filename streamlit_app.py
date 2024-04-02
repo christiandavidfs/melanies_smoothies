88358970aa183faed2d9c0746ae0b4ba6c094e9f
christiandavidfs <christiandavid.fs@gmail.com>
2024-04-02 14:20:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ ingredients_list = st.multiselect (
 #new section
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
+#st.text(fruityvice_response)
+fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
 
